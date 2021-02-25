a9dc90e2e8977061e077b2cd42d9cca10e315ac8
Memphis <bixthegamedev@gmail.com>
2021-02-24 23:05:14 -0500

    Added elevator enviorment and buttons to end the game

---
+++ Assets/ElevatorButtonScript.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class ElevatorButtonScript : MonoBehaviour
+{
+    private bool allowedTap = true;
+    public GameManager manager;
+    public UnityEvent Event = new UnityEvent();
+    
+
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.name == "UI Interactor" && allowedTap)
+        {
+            allowedTap = false;
+            Invoke("cooldown", 1f);
+            manager.GameOverVRWin();
+
+        }
+    }
+    void cooldown()
+    {
+        allowedTap = true;
+    }
+}
